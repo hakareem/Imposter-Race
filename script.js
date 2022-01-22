31c9b860f719f04c2f5f3bdd65589d400227cc65
hakareem <harithakareem72.com>
2022-01-22 18:30:53 +0000

    change

--- script.js
+++ script.js
@@ -8,30 +8,22 @@ let colors = [
   "purple",
   "blue",
   "pink",
+  "orange",
   "brown",
   "grey",
-  "lime",
-  "teal",
-  "maroon",
-  "wheat",
-  "orange",
-  "navy",
-  "silver",
-  "dark blue",
-  "chocolate",
-  "bronze",
-  "golden",
-  "aqua",
-  "azure",
+  "yellow",
+  "white",
 ];
 
-const numberOfImposters = 14;
+const numberOfImposters = 7;
 
+document.getElementById("btn")
 for (let i = 0; i < numberOfImposters; i++) {
   imposterDiv[i] = document.createElement("div");
   document.body.appendChild(imposterDiv[i]);
   imposterDiv[i].classList.add("imposter");
-  imposterDiv[i].style.top = i * 7 + "%";
+  imposterDiv[i].style.top = i * 15 + "%";
+  // imposterDiv[i].style.bottom = i * 4 + "%";
   imposterDiv[i].style.backgroundColor = colors[i];
   imposterXPosition[i] = 0;
 }
@@ -44,15 +36,18 @@ let restartBtn = function () {
   }
 };
 
+
 function imposterTravel() {
   for (let i = 0; i < numberOfImposters; i++) {
     imposterDiv[i].style.left = imposterXPosition[i] + "%";
     imposterXPosition[i] += Math.random();
 
-    if (imposterXPosition[i] > 97) {
+    if (imposterXPosition[i] > 96) {
       alert(`The ${colors[i]} imposter won`);
       restartBtn();
     }
   }
   requestAnimationFrame(imposterTravel);
 }
+
+
